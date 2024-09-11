00ee21290df0346ba9176a3d2fa26528004fb8b8
Aidan Carr <you@example.com>
2024-09-11 18:12:23 -0400

    [12] memory class has a function to set memory on a given string of hex, work in progress

--- distrib/host/memory.js
+++ distrib/host/memory.js
@@ -11,11 +11,20 @@ var TSOS;
             this.mainMemory = mainMemory;
             this.initializeMemoryArray();
         }
-        //create memory in array form: space of 0xffff, word size 0xff 
+        //create mainMemory in array form: space of 0xffff 
         initializeMemoryArray() {
             this.mainMemory = new Array(0xffff);
             //initialize to all 0x00's
-            //this.reset()
+            this.reset();
+        }
+        //(re)set all mainMemory elements to 0x00
+        reset() {
+            for (let i = 0x0000; i < 0xffff; i++) {
+                this.mainMemory[i] = (0x00);
+            }
+        }
+        //given a starting position and string list of hexes, set memory elements to a given hex
+        setMemoryStr(hexList, startIndex) {
         }
     }
     TSOS.Memory = Memory;
--- source/host/memory.ts
+++ source/host/memory.ts
@@ -12,12 +12,24 @@ module TSOS {
             this.initializeMemoryArray();
         }
 
-        //create memory in array form: space of 0xffff, word size 0xff 
+        //create mainMemory in array form: space of 0xffff 
         public initializeMemoryArray(){
             this.mainMemory = new Array(0xffff);
             
             //initialize to all 0x00's
-            //this.reset()
+            this.reset()
+        }
+
+        //(re)set all mainMemory elements to 0x00
+        public reset(){
+            for(let i = 0x0000; i < 0xffff; i++){
+                this.mainMemory[i] = (0x00);
+            }  
+        }
+
+        //given a starting position and string list of hexes, set memory elements to a given hex
+        public setMemoryStr(hexList: string[], startIndex: number){
+            
         }
     }
 }
