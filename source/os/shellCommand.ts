f86736f287d261c9daf3ee7c5ae3a2669e23a979
Aidan Carr <89876013+AidanCarr1@users.noreply.github.com>
2025-04-14 10:42:44 -0400

    Delete source/os/shellCommand.ts

--- source/os/shellCommand.ts
+++
@@ -1,9 +0,0 @@
-module TSOS {
-    export class ShellCommand {
-        constructor(public func: any,
-                    public command: string = "",
-                    public description: string = "", //for help command
-                    public manual: string = "") { //for man command
-        }
-    }
-}
